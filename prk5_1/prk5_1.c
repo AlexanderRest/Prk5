5a83da63760135ad706dacb4b279e73e80d98721
AlexanderRest <restzone0964@gmail.com>
2025-04-18 12:42:30 +0300

    2nd commit

--- prk5_1/prk5_1.c
+++ prk5_1/prk5_1.c
@@ -4,37 +4,63 @@
 #include <signal.h>
 #include <time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdbool.h>
+#include <sys/time.h>
 
-volatile sig_atomic_t guessed_number = 0;
-volatile sig_atomic_t attempts = 0;
+#define MAX_ROUNDS 10
+
+typedef struct {
+    int number;
+    int attempts;
+    struct timeval start_time;
+} GameData;
+
+volatile sig_atomic_t current_role = 0; // 0 - родитель загадывает, 1 - ребенок загадывает
 volatile sig_atomic_t game_over = 0;
 volatile sig_atomic_t current_guess = 0;
+volatile sig_atomic_t response_received = 0;
+volatile sig_atomic_t rounds_played = 0;
 pid_t other_pid;
 
-void player1_handler(int sig, siginfo_t *info, void *context) {
+void thinker_handler(int sig, siginfo_t *info, void *context) {
+    (void)context;
     if (sig == SIGRTMIN) {
         current_guess = info->si_value.sival_int;
-        attempts++;
-        if (current_guess == guessed_number) {
-            union sigval value;
-            value.sival_int = 1; // correct guess
+        GameData* data = (GameData*)info->si_value.sival_ptr;
+        data->attempts++;
+
+        union sigval value;
+        if (current_guess == data->number) {
+            struct timeval end_time;
+            gettimeofday(&end_time, NULL);
+            long seconds = end_time.tv_sec - data->start_time.tv_sec;
+            long microseconds = end_time.tv_usec - data->start_time.tv_usec;
+            double elapsed = seconds + microseconds*1e-6;
+
+            value.sival_int = data->attempts;
+            printf("Угадано за %d попыток! Время: %.3f сек.\n", data->attempts, elapsed);
             sigqueue(other_pid, SIGUSR1, value);
             game_over = 1;
+            current_role = !current_role;
         } else {
-            union sigval value;
-            value.sival_int = 0; // wrong guess
+            value.sival_int = 0;
             sigqueue(other_pid, SIGUSR2, value);
         }
     }
 }
 
-void player2_handler(int sig, siginfo_t *info, void *context) {
+void guesser_handler(int sig, siginfo_t *info, void *context) {
+    (void)context;
     if (sig == SIGUSR1) {
-        printf("Player2: Correct! Number guessed in %d attempts.\n", attempts);
+        printf("Успех! Число угадано за %d попыток.\n", info->si_value.sival_int);
         game_over = 1;
+        current_role = !current_role;
+        rounds_played++;
+        response_received = 1;
     } else if (sig == SIGUSR2) {
-        printf("Player2: Wrong guess (%d). Try again.\n", current_guess);
+        printf("Не угадал. Пробую снова...\n");
+        response_received = 1;
     }
 }
 
@@ -46,31 +72,42 @@ void setup_signal_handlers(void (*handler)(int, siginfo_t *, void *), int sig) {
     sigaction(sig, &sa, NULL);
 }
 
-void play_as_player1(int max_number) {
-    srand(time(NULL) ^ getpid());
-    guessed_number = rand() % max_number + 1;
-    printf("Player1: I've guessed a number between 1 and %d. Let's play!\n", max_number);
-    
+void play_as_thinker(int max_number) {
+    GameData data;
+    data.number = rand() % max_number + 1;
+    data.attempts = 0;
+    gettimeofday(&data.start_time, NULL);
+
+    printf("Загадал число от 1 до %d. Ожидаю попыток...\n", max_number);
+
     while (!game_over) {
         pause();
     }
-    
-    printf("Player1: Game over. The number was %d.\n", guessed_number);
 }
 
-void play_as_player2(int max_number) {
-    int guess;
-    srand(time(NULL) ^ getpid());
-    
+void play_as_guesser(int max_number) {
+    GameData data;
+    data.attempts = 0;
+    gettimeofday(&data.start_time, NULL);
+
     while (!game_over) {
-        guess = rand() % max_number + 1;
-        printf("Player2: Guessing %d...\n", guess);
-        
+        data.attempts++;
+        data.number = rand() % max_number + 1;
+        printf("Попытка %d: %d\n", data.attempts, data.number);
+
         union sigval value;
-        value.sival_int = guess;
-        sigqueue(other_pid, SIGRTMIN, value);
-        
-        pause(); // Wait for response
+        value.sival_ptr = &data;
+        response_received = 0;
+
+        if (sigqueue(other_pid, SIGRTMIN, value) == -1) {
+            perror("sigqueue");
+            break;
+        }
+
+        // Ждем ответа
+        while (!response_received && !game_over) {
+            pause();
+        }
     }
 }
 
@@ -79,48 +116,69 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <max_number>\n", argv[0]);
         return 1;
     }
-    
+
     int max_number = atoi(argv[1]);
     if (max_number <= 1) {
         fprintf(stderr, "Max number must be greater than 1\n");
         return 1;
     }
-    
+
+    srand(time(NULL));
+
+    // Блокируем сигналы перед fork
+    sigset_t mask, oldmask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGRTMIN);
+    sigaddset(&mask, SIGUSR1);
+    sigaddset(&mask, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
     pid_t pid = fork();
-    
+
     if (pid == -1) {
         perror("fork");
         return 1;
     }
-    
-    if (pid == 0) { // Child process (Player2)
+
+    if (pid == 0) { // Дочерний процесс
         other_pid = getppid();
-        setup_signal_handlers(player2_handler, SIGUSR1);
-        setup_signal_handlers(player2_handler, SIGUSR2);
-        
-        for (int i = 0; i < 10; i++) {
-            game_over = 0;
-            attempts = 0;
-            printf("\nRound %d: Player2 is guessing...\n", i+1);
-            play_as_player2(max_number);
-            sleep(1); // Small delay before switching roles
+
+        while (rounds_played < MAX_ROUNDS) {
+            if (current_role) {
+                setup_signal_handlers(thinker_handler, SIGRTMIN);
+                printf("\n=== Раунд %d ===\n", rounds_played + 1);
+                printf("Я - Игрок 2 (загадывающий)\n");
+                play_as_thinker(max_number);
+            } else {
+                setup_signal_handlers(guesser_handler, SIGUSR1);
+                setup_signal_handlers(guesser_handler, SIGUSR2);
+                printf("\n=== Раунд %d ===\n", rounds_played + 1);
+                printf("Я - Игрок 2 (угадывающий)\n");
+                play_as_guesser(max_number);
+            }
         }
-    } else { // Parent process (Player1)
+        _exit(0);
+    } else { // Родительский процесс
         other_pid = pid;
-        setup_signal_handlers(player1_handler, SIGRTMIN);
-        
-        for (int i = 0; i < 10; i++) {
-            game_over = 0;
-            attempts = 0;
-            printf("\nRound %d: Player1 is guessing...\n", i+1);
-            play_as_player1(max_number);
-            sleep(1); // Small delay before switching roles
+
+        while (rounds_played < MAX_ROUNDS) {
+            if (!current_role) {
+                setup_signal_handlers(thinker_handler, SIGRTMIN);
+                printf("\n=== Раунд %d ===\n", rounds_played + 1);
+                printf("Я - Игрок 1 (загадывающий)\n");
+                play_as_thinker(max_number);
+            } else {
+                setup_signal_handlers(guesser_handler, SIGUSR1);
+                setup_signal_handlers(guesser_handler, SIGUSR2);
+                printf("\n=== Раунд %d ===\n", rounds_played + 1);
+                printf("Я - Игрок 1 (угадывающий)\n");
+                play_as_guesser(max_number);
+            }
         }
-        
-        // Send termination signal to child
+
         kill(pid, SIGTERM);
-        wait(NULL); // Wait for child to terminate
+        wait(NULL);
     }
-    
+
     return 0;
 }
--- prk5_2/prk5_2.c
+++ prk5_2/prk5_2.c
@@ -4,18 +4,24 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <time.h>
 #include <signal.h>
-#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include <sys/time.h>
 
+#define MAX_ROUNDS 10
 #define FIFO1 "/tmp/guess_number_fifo1"
 #define FIFO2 "/tmp/guess_number_fifo2"
 
 typedef struct {
     int number;
-    bool is_guess;
-    bool game_over;
-} Message;
+    int attempts;
+    int is_guess;
+    int game_over;
+    struct timeval start_time;
+} GameMessage;
+
+volatile sig_atomic_t terminate = 0;
 
 void cleanup() {
     unlink(FIFO1);
@@ -23,130 +29,143 @@ void cleanup() {
 }
 
 void handle_signal(int sig) {
+    terminate = 1;
     cleanup();
-    exit(0);
 }
 
-void play_as_player1(int max_number, int read_fd, int write_fd) {
-    srand(time(NULL) ^ getpid());
-    int secret_number = rand() % max_number + 1;
-    printf("Player1: I've guessed a number between 1 and %d. Let's play!\n", max_number);
-    
-    Message msg;
-    int attempts = 0;
-    
-    while (1) {
-        read(read_fd, &msg, sizeof(Message));
-        
-        if (msg.game_over) {
-            break;
-        }
-        
-        attempts++;
-        msg.is_guess = (msg.number == secret_number);
-        msg.game_over = msg.is_guess;
-        
-        write(write_fd, &msg, sizeof(Message));
-        
-        if (msg.is_guess) {
-            printf("Player1: Correct guess! The number was %d. Attempts: %d\n", secret_number, attempts);
-            break;
-        } else {
-            printf("Player1: Received guess %d (wrong)\n", msg.number);
+void create_fifos() {
+    mkfifo(FIFO1, 0666);
+    mkfifo(FIFO2, 0666);
+}
+
+void play_thinker(int max_number, int read_fd, int write_fd) {
+    GameMessage msg;
+    msg.number = rand() % max_number + 1;
+    msg.attempts = 0;
+    msg.is_guess = 0;
+    msg.game_over = 0;
+    gettimeofday(&msg.start_time, NULL);
+
+    printf("Загадал число от 1 до %d\n", max_number);
+    write(write_fd, &msg, sizeof(GameMessage));
+
+    while (!terminate) {
+        if (read(read_fd, &msg, sizeof(GameMessage)) > 0) {
+            if (msg.game_over) break;
+
+            msg.attempts++;
+            if (msg.number == msg.number) {
+                struct timeval end_time;
+                gettimeofday(&end_time, NULL);
+                double elapsed = (end_time.tv_sec - msg.start_time.tv_sec) +
+                               (end_time.tv_usec - msg.start_time.tv_usec) / 1000000.0;
+
+                printf("Число %d угадано за %d попыток (время: %.3f сек.)\n",
+                       msg.number, msg.attempts, elapsed);
+
+                msg.is_guess = 1;
+                msg.game_over = 1;
+                write(write_fd, &msg, sizeof(GameMessage));
+                break;
+            } else {
+                msg.is_guess = 0;
+                write(write_fd, &msg, sizeof(GameMessage));
+            }
         }
     }
 }
 
-void play_as_player2(int max_number, int read_fd, int write_fd) {
-    srand(time(NULL) ^ getpid());
-    Message msg;
-    int attempts = 0;
-    
-    while (1) {
-        attempts++;
-        msg.number = rand() % max_number + 1;
-        msg.is_guess = false;
-        msg.game_over = false;
-        
-        printf("Player2: Guessing %d...\n", msg.number);
-        write(write_fd, &msg, sizeof(Message));
-        
-        read(read_fd, &msg, sizeof(Message));
-        
-        if (msg.game_over) {
-            if (msg.is_guess) {
-                printf("Player2: I won! Number %d guessed in %d attempts.\n", msg.number, attempts);
+void play_guesser(int max_number, int read_fd, int write_fd) {
+    GameMessage msg;
+
+    while (!terminate) {
+        if (read(read_fd, &msg, sizeof(GameMessage)) > 0) {
+            if (msg.game_over) break;
+
+            msg.attempts++;
+            msg.number = rand() % max_number + 1;
+            printf("Попытка %d: %d\n", msg.attempts, msg.number);
+
+            write(write_fd, &msg, sizeof(GameMessage));
+
+            if (read(read_fd, &msg, sizeof(GameMessage)) > 0) {
+                if (msg.is_guess) {
+                    printf("Угадал число %d!\n", msg.number);
+                    break;
+                }
             }
-            break;
         }
     }
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <max_number>\n", argv[0]);
+        fprintf(stderr, "Использование: %s <макс_число>\n", argv[0]);
         return 1;
     }
-    
+
     int max_number = atoi(argv[1]);
     if (max_number <= 1) {
-        fprintf(stderr, "Max number must be greater than 1\n");
+        fprintf(stderr, "Число должно быть больше 1\n");
         return 1;
     }
-    
-    // Set up signal handler for cleanup
+
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
-    
-    // Create FIFOs
-    mkfifo(FIFO1, 0666);
-    mkfifo(FIFO2, 0666);
-    
+
+    srand(time(NULL));
+    cleanup();
+    create_fifos();
+
     pid_t pid = fork();
-    
+
     if (pid == -1) {
         perror("fork");
-        cleanup();
         return 1;
     }
-    
-    if (pid == 0) { // Child process (Player2)
+
+    if (pid == 0) { // Дочерний процесс (Игрок 2)
         int fd_read = open(FIFO1, O_RDONLY);
         int fd_write = open(FIFO2, O_WRONLY);
-        
-        for (int i = 0; i < 10; i++) {
-            printf("\nRound %d: Player2 is guessing...\n", i+1);
-            play_as_player2(max_number, fd_read, fd_write);
-            sleep(1);
+
+        for (int i = 0; i < MAX_ROUNDS && !terminate; i++) {
+            printf("\n=== Раунд %d ===\n", i+1);
+
+            if (i % 2 == 0) {
+                printf("Я - Игрок 2 (угадываю)\n");
+                play_guesser(max_number, fd_read, fd_write);
+            } else {
+                printf("Я - Игрок 2 (загадываю)\n");
+                play_thinker(max_number, fd_read, fd_write);
+            }
         }
-        
-        // Send termination message
-        Message msg = {0, false, true};
-        write(fd_write, &msg, sizeof(Message));
-        
+
         close(fd_read);
         close(fd_write);
-    } else { // Parent process (Player1)
+        _exit(0);
+    } else { // Родительский процесс (Игрок 1)
         int fd_write = open(FIFO1, O_WRONLY);
         int fd_read = open(FIFO2, O_RDONLY);
-        
-        for (int i = 0; i < 10; i++) {
-            printf("\nRound %d: Player1 is guessing...\n", i+1);
-            play_as_player1(max_number, fd_read, fd_write);
-            sleep(1);
+
+        for (int i = 0; i < MAX_ROUNDS && !terminate; i++) {
+            printf("\n=== Раунд %d ===\n", i+1);
+
+            if (i % 2 == 0) {
+                printf("Я - Игрок 1 (загадываю)\n");
+                play_thinker(max_number, fd_read, fd_write);
+            } else {
+                printf("Я - Игрок 1 (угадываю)\n");
+                play_guesser(max_number, fd_read, fd_write);
+            }
         }
-        
-        // Send termination message
-        Message msg = {0, false, true};
-        write(fd_write, &msg, sizeof(Message));
-        
+
+        kill(pid, SIGTERM);
+        wait(NULL);
+
         close(fd_read);
         close(fd_write);
-        
-        // Wait for child to finish
-        wait(NULL);
         cleanup();
     }
-    
+
     return 0;
 }
